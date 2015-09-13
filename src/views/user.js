6f1b5f57c38aa1035dcca9684c9cfb81ce78f553
Brian R. Bondy <netzen@gmail.com>
2015-09-13 10:51:49 -0400

    Set community wiki for user display when user id is null

--- src/views/user.js
+++ src/views/user.js
@@ -4,8 +4,11 @@ import {userUrl, seUserUrl} from '../siteUtil.js';
 var User = React.createClass({
   render: function() {
     return <div><span>Posted by: </span>
-      <a href={userUrl(this.props.siteSlug, this.props.lang, this.props.id)}>{this.props.displayName}</a>
-      <a href={seUserUrl(this.props.siteSlug, this.props.id)}><span className="se-backlink fa fa-stack-overflow"/></a>
+      { !this.props.id ? <span>community wiki</span> : <span>
+          <a href={userUrl(this.props.siteSlug, this.props.lang, this.props.id)}>{this.props.displayName}</a>
+          <a href={seUserUrl(this.props.siteSlug, this.props.id)}><span className="se-backlink fa fa-stack-overflow"/></a>
+      </span>
+      }
   </div>;
   }
 });
