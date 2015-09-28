2741308d5eb3163efbaaee752a5fb8b1bf4398c9
Brian R. Bondy <netzen@gmail.com>
2015-09-27 21:18:18 -0400

    Visual tweaks

--- src/views/navigationBar.js
+++ src/views/navigationBar.js
@@ -5,8 +5,8 @@ var NavigationBar = React.createClass({
   render: function() {
     return <ul className='navigationBar'>
       <li><a href={questionUrl(this.props.siteSlug, this.props.lang)}>Questions</a></li>
-      <li><a href={userUrl(this.props.siteSlug, this.props.lang)}>Users</a></li>
       <li><a href={tagsUrl(this.props.siteSlug, this.props.lang)}>Tags</a></li>
+      <li><a href={userUrl(this.props.siteSlug, this.props.lang)}>Users</a></li>
     </ul>;
   }
 });
